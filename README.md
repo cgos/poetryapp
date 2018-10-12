f285b27d5a36c4bee6108bf69ada86ad79a61030
Christian Gosselin <christian.gosselin@bnc.ca>
2018-10-12 15:41:36 -0400

    gateway

--- README.md
+++ README.md
@@ -18,8 +18,18 @@ Whenever you're done you can delete all installed docker container by killing th
 # Minikube
 Follow minikube documentation for installation
 
-Start minikube: ``` minikube start --vm-driver docker-machine-driver-hyperkit``` and validate it's running fine: ``` kubectl get nodes ```
+Start minikube: ``` minikube start --vm-driver docker-machine-driver-hyperkit --cpus 4 --memory 8192``` and validate it's running fine: ``` kubectl get nodes ```
 
 
 Whenever you're done you can stop minikube: ```minikube stop```
 
+On osx if cpu starts peaking try shutting down minikube dashboard:
+```
+k get pods -n kube-system -o name
+and kill the dashboard
+k delete pods -n kube-system kubernetes-dashboard-5498ccf677-9rg8v
+```
+
+# Run in kubernetes
+
+We are using a deployment spec that can be installed for each application using the following command: ```k apply -f deployment-poetrywebapp.yaml ```
--- poemspringgateway/src/main/java/poemspringgateway/gateway/GatewayApplication.java
+++ poemspringgateway/src/main/java/poemspringgateway/gateway/GatewayApplication.java
@@ -2,9 +2,23 @@ package poemspringgateway.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		// @formatter:off
+		return builder.routes()
+				.route("poem", r -> r.path("/poem").uri("http://localhost:8181"))
+				.route("bio", r -> r.path("/poetbio").uri("http://localhost:8888"))
+				.route("poetry", r -> r.path("/poetry").uri("http://localhost:8080"))
+				.build();
+		// @formatter:on
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
